f90a273cd9417b7ae352b6b6309ba596b5d54fbe
Mohamed Bedair <mbedairgis@gmail.com>
2020-08-18 16:10:26 +0300

    Changed the class system.java to manage.java to avoid confusion with the class System

---
+++ src/Func/Manage.java
@@ -0,0 +1,15 @@
+package Func;
+
+import java.util.ArrayList;
+
+public class Manage {
+    static public ArrayList<Admin> admins = new ArrayList<Admin>();
+    static public ArrayList<Doctor> doctors = new ArrayList<Doctor>();
+    static public ArrayList<Receptionist> Receptionists = new ArrayList<Receptionist>();
+    static public ArrayList<Patient> patients = new ArrayList<Patient>();
+    static public ArrayList<Nurse> nurses = new ArrayList<Nurse>();
+    static public ArrayList<Appointment> appointments = new ArrayList<Appointment>();
+    static public ArrayList<Case> cases = new ArrayList<Case>();
+
+
+}
