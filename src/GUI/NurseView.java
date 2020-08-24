517c957a0690d0000b03e9fe8f14b856d3bb13f1
Mohamed Bedair <mbedairgis@gmail.com>
2020-08-24 20:37:03 +0300

    Fixed Background images

--- src/GUI/NurseView.java
+++ src/GUI/NurseView.java
@@ -1,4 +1,23 @@
 package GUI;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.image.ImageView;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class NurseView {
+    public Button logOutBtn;
+    public ImageView img;
+
+    public void logOut(ActionEvent actionEvent) throws IOException {
+        Parent viewParent = FXMLLoader.load(getClass().getResource("LoginView.fxml"));
+        Scene viewScene = new Scene(viewParent);
+        Stage primaryStage = (Stage) logOutBtn.getScene().getWindow();
+        primaryStage.setScene(viewScene);
+    }
 }
